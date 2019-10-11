782d87915ea7688d3c67ae757502bf22adb54816
Rogério Fernandes <rogerio.fernandes@thoughtworks.com>
2019-10-11 15:15:55 -0300

    #3 [RF/CM] refactoring for observables

--- src/app/pages/adopt/adopt.component.ts
+++ src/app/pages/adopt/adopt.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { Pokemon } from '../../models/pokemon.model';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'poke-adopt',
@@ -8,12 +9,11 @@ import { Pokemon } from '../../models/pokemon.model';
   styleUrls: ['./adopt.component.scss']
 })
 export class AdoptComponent implements OnInit {
-  public pokemons: Pokemon[] = [];
+  public pokemons: Observable<Pokemon[]> = of([]);
 
   constructor(private readonly pokemonService: PokemonService) { }
 
   ngOnInit() {
-    this.pokemonService.getAll()
-      .subscribe((pokemons) => this.pokemons = pokemons);
+    this.pokemons = this.pokemonService.getAll();
   }
 }
