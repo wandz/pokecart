8d623e7a6fea08b843aaae6363c0837d42c33a72
Paula Botelho <paula.botelho@outlook.com>
2019-10-16 18:24:10 -0300

    #6 [RF/PB] fixing router reference

--- src/app/pages/adopt/adopt.component.ts
+++ src/app/pages/adopt/adopt.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { Pokemon } from '../../models/pokemon.model';
-import {Observable, of} from 'rxjs';
-import {catchError} from 'rxjs/operators';
-import {Router} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'poke-adopt',
@@ -13,12 +12,17 @@ import {Router} from '@angular/router';
 export class AdoptComponent implements OnInit {
   public pokemons: Observable<Pokemon[]> = of([]);
 
-  constructor(private readonly pokemonService: PokemonService, private readonly router: Router) { }
+  constructor(private readonly pokemonService: PokemonService, private readonly router: Router) {
+  }
 
   ngOnInit() {
     this.pokemons = this.pokemonService.getAll();
     this.pokemons.subscribe({
       error: () => this.router.navigate(['500'])
     });
+  }
+
+  goToCart() {
+    this.router.navigate(['cart']);
   }
 }
